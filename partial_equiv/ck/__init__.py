cb41ae59737038bc92da2e48d227c9a9bf2bd58a
Tycho van der Ouderaa <tychovdo@gmail.com>
2021-12-22 17:14:54 +0000

    fix

--- partial_equiv/ck/__init__.py
+++ partial_equiv/ck/__init__.py
@@ -1,8 +1,5 @@
 from .mfn import GaborNet, FourierNet
 from .magnet import MAGNet
 from .siren import SIREN
-from .rfn import RFN
 from .mlp import MLP
-from .lrf import LRF
-from .lrf2 import LRF2
 from .siren2 import SIREN2
