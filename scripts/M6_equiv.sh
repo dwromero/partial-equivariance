fb64bb9e8644ad69c6305a0e74dc4ccb279594aa
Tycho van der Ouderaa <tychovdo@gmail.com>
2022-01-11 23:25:31 +0000

    script files

---
+++ scripts/M6_equiv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ..
+python main.py base_group.name=SE2 base_group.no_samples=8 base_group.sample_per_batch_element=False base_group.sample_per_layer=True base_group.sampling_method=random conv.bias=True conv.padding=same conv.partial_equiv=False dataset=MNIST6-180 kernel.no_hidden=32 kernel.no_layers=3 kernel.size=7 kernel.type=SIREN kernel.learn_omega0=False kernel.omega0=10.0 net.dropout=0 net.no_blocks=2 net.no_hidden=10 net.norm=BatchNorm net.pool_blocks=[1,2] net.type=CKResNet seed=0 train.batch_size=64 train.do=True train.epochs=300 train.lr=0.001
